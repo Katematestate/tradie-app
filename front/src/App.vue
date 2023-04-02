e5aee0149133ec91b845c882d252e0a6bc1e49eb
shayelock <111708175+shayelock@users.noreply.github.com>
2023-04-03 09:33:17 +1200

    g

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,6 @@
+<script setup>
+
+</script>
 <template>
   <router-view></router-view>
 </template>
---
+++ front/src/views/TradieAccountPage.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+<template>
+<div class="business-profile">
+
+ <div class="business-header">
+             <h1> Business Profile</h1>
+  </div>
+
+    <div class="business-info">
+
+            <div class="info-section-1">  
+                <h1>section 1</h1>
+            </div>
+            <div class="info-section-2">  
+                <h1>section 2</h1>
+            </div>
+            <div class="info-section-3">  
+                <h1>section 3</h1>
+           </div>
+
+    </div>
+
+
+</div>
+</template>
+<style scoped>
+.business-profile {
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
+<script>
+
+</script>
