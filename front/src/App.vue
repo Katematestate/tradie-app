afa4704071925bbe7011b9550859e81019b87b38
kelsiefromyoobee <kelsie.baigent@gmail.com>
2023-04-03 11:54:44 +1200

    completed buttons

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,8 @@
 <script setup>
-import Buttons from './components/Button.vue';
 </script>
 
 <template>
   <router-view></router-view>
-  <Buttons label="asdf" type="cta"/>
-  <Buttons label="ghjkl" type="secondary"/>
 </template>
   
 <style scoped>
