d3ed68fb1a8f9f651c192fbfb139a8a9ff93f5e2
kelsiefromyoobee <kelsie.baigent@gmail.com>
2023-04-06 09:30:56 +1200

    added template

--- front/src/views/TradieJobRequests.vue
+++ front/src/views/TradieJobRequests.vue
@@ -1,30 +1,10 @@
 
 <template>
+    <TradieJobProcess/>
     <div class="card">
         <DataView :value="products" paginator :rows="5">
             <template #list="slotProps">
-                <div class="col-12">
-                    <div class="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-                        <img class="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" :src="`https://primefaces.org/cdn/primevue/images/product/${slotProps.data.image}`" :alt="slotProps.data.name" />
-                        <div class="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-                            <div class="flex flex-column align-items-center sm:align-items-start gap-3">
-                                <div class="text-2xl font-bold text-900">{{ slotProps.data.name }}</div>
-                                <Rating :modelValue="slotProps.data.rating" readonly :cancel="false"></Rating>
-                                <div class="flex align-items-center gap-3">
-                                    <span class="flex align-items-center gap-2">
-                                        <i class="pi pi-tag"></i>
-                                        <span class="font-semibold">{{ slotProps.data.category }}</span>
-                                    </span>
-                                    <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data)"></Tag>
-                                </div>
-                            </div>
-                            <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                                <span class="text-2xl font-semibold">${{ slotProps.data.price }}</span>
-                                <Button icon="pi pi-shopping-cart" rounded :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"></Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                div.
             </template>
         </DataView>
     </div>
@@ -32,4 +12,5 @@
 
 <script setup>
 import DataView from 'primevue/dataview';
+import TradieJobProcess from '../components/TradieJobProcess.vue';
 </script>
