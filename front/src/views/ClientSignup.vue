ba8de31f806dba7b8f78ab423677c606c165b97e
thewhitemj <113862023+thewhitemj@users.noreply.github.com>
2023-04-06 10:07:17 +1200

    Update ClientSignup.vue

--- front/src/views/ClientSignup.vue
+++ front/src/views/ClientSignup.vue
@@ -14,76 +14,82 @@ import Checkbox from 'primevue/checkbox';
 <div class="client-signup">
     <div class="margin1 flex">
         <div class="links-div">
-        <router-link to='/' class="spacing-large">Go to Home</router-link>
-    </div>
+            <router-link to='/' class="spacing-large link">Go to Home</router-link>
+        </div>
     </div>
 
 
     <div class="flex">
         <div class="flex-col">
 
+            <div class="heading-container">
+                <div class="spacing-large align-items-center">
+                    <h1>Sign Up</h1>
+                </div>
+            </div>
 
-            <div class="">
-                <h1>Sign Up</h1>
+            <div class="text-box-container">
+                <div class="spacing-standard">
+                    <p>First Name</p>
+                    <InputText v-model="Fname" class="input-class-box" placeholder ="First Name"/>
+                </div>
+
+
+                <div class="spacing-standard">
+                    <p>Last Name</p>
+                    <InputText v-model="Lname" class="input-class-box" placeholder ="Last Name"/>
+                </div>
+
+
+                <div class="spacing-standard">
+                    <p>Location</p>
+                    <InputText v-model="location" class="input-class-box align-items-center" placeholder ="Location"/>
+                </div>
+
+
+                <div class="spacing-standard">
+                    <p>Email</p>
+                    <InputText v-model="email" class="input-class-box" placeholder ="Email"/>
+                </div>
+
+
+                <div class="spacing-standard">
+                    <p>Password</p>
+                    <InputText v-model="password" class="input-class-box" placeholder ="Password"/>
+                </div>
+
+
+                <div class="spacing-standard">
+                    <p>Confirm Password</p>
+                    <InputText class="input-class-box" placeholder ="Confirm Password"/>
+                </div>
             </div>
 
 
-            <div class="spacing-standard">
-                <p>First Name</p>
-                <InputText class="input-class-box" placeholder ="First Name"/>
+            <div class="checkbox-container">
+                <div class="spacing-standard flex-row">
+                    <Checkbox class="" v-model="tos_concent"/>
+                    <p>Do You Consent To The Terms Of Service</p>
+                </div>
+            </div>
+            
+
+            <div class="checkbox-container">
+                <div class="spacing-standard flex-row">
+                    <Checkbox class="" v-model="email_concent"  />
+                    <p>Do You Concent To Recieving Emails From Find A Tradie</p>
+                </div>
             </div>
 
-
-            <div class="spacing-standard">
-                <p>Last Name</p>
-                <InputText class="input-class-box" placeholder ="Last Name"/>
+            
+            <div class="checkbox-container">
+                <div class="spacing-standard">
+                    <p>Insert Kelsie's Button Here</p>
+                </div>
             </div>
 
-
-            <div class="spacing-standard">
-                <p>Location</p>
-                <InputText class="input-class-box aic" placeholder ="Location"/>
-            </div>
-
-
-            <div class="spacing-standard">
-                <p>Email</p>
-                <InputText class="input-class-box" placeholder ="Email"/>
-            </div>
-
-
-            <div class="spacing-standard">
-                <p>Password</p>
-                <InputText class="input-class-box" placeholder ="Password"/>
-            </div>
-
-
-            <div class="spacing-standard">
-                <p>Confirm Password</p>
-                <InputText class="input-class-box" placeholder ="Confirm Password"/>
-            </div>
-
-
-            <div class="spacing-standard flex-row">
-                <Checkbox class="p-checkbox p-checkbox-box p-checkbox-icon" v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-                <p>Do You Consent</p>
-            </div>
-
-
-            <div class="spacing-standard flex-row">
-                <Checkbox class="p-checkbox p-checkbox-box p-checkbox-icon" v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
-                <p>Do You Consent</p>
-            </div>
-
-            <div class="spacing-standard">
-                <p>Insert Kelsie's Button Here</p>
-            </div>
 
         </div>
-    </div>
-
-    <div class="margin2 flex">
-        <!-- <h1> Here is Margin 2</h1> -->
     </div>
 
 </div>
@@ -91,29 +97,61 @@ import Checkbox from 'primevue/checkbox';
     <h1>CLIENT SIGN UP FOOTER AREA</h1>
 </template>
     
+
+
+<script>
+export default {
+  data(){ // data itself starts
+    return{ // data variables starts
+    
+    } // data variables end
+  }, // data itself ends
+  methods: {
+          
+    },
+
+}
+</script>
+
+
 <style scoped lang="scss">
 p{
     margin: 0;
+    font-family: var(--font-secondary);
 }
 h1{
     font-family: var(--font-main);
 }
-
-.client-signup{
-    display: grid;
-    grid-template-columns: repeat(3,1fr);
-    gap: var(--spacing-standard);
-    border: 3px solid magenta;
+.heading-container{
+max-width: 800px;
+margin: 0 auto;
 }
+.text-box-container{
+max-width: 500px;
+width: 100%;
+margin: 0 auto;
+}
+.checkbox-container{
+max-width: 500px;
+margin: 0 auto;
+}
+// .align-items-center{
+//     align-items: center;
+// }
+.link{
+    color: black;
+}
+
 .input-class-box{
     border-radius: 5px;
     border: none;
-    padding-top: 3%;
-    padding-left: 3% ;
-    padding-right: 5%;
-    padding-bottom: 10%;
+    // padding-top: 3%;
+    // padding-left: 3% ;
+    // padding-right: 5%;
+    // padding-bottom: 10%;
     background-color: #eeeeee;
     width: 100%;
+
 }
 .flex-row{
     display: flex;
@@ -123,15 +161,8 @@ h1{
 .flex-col{
     display: flex;
     flex-direction: column;
-    align-items: center;
     flex: 1;
 
-}
-
-.p-checkbox{
-    height: 20px;
-    width: 20px;
-    border: 2px solid black;
 }
 .flex{
     display: flex;
@@ -145,6 +176,6 @@ padding-bottom: var(--spacing-small);
 padding-bottom: var(--spacing-standard);
 }
 .spacing-large{
-padding: var(--spacing-large);
+margin: var(--spacing-large);
 }
 </style>
