b42fe83e25400def1271063de1b22627e013ac0f
Katematestate <80445606+Katematestate@users.noreply.github.com>
2023-04-05 17:20:54 +1200

    Fix link box model

--- front/src/components/BackToLink.vue
+++ front/src/components/BackToLink.vue
@@ -3,7 +3,7 @@ defineProps({ to: String, label: String });
 </script>
 
 <template>
-  <router-link :to="to" class="flex align-items-center gap-1">
+  <router-link :to="to" class="inline-flex align-items-center gap-1">
     <i class="pi pi-chevron-left"></i><span>{{ label }}</span>
   </router-link>
 </template>
