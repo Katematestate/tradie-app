f5fec574340bd3642864088f3d14df8f397397cf
thewhitemj <113862023+thewhitemj@users.noreply.github.com>
2023-04-07 21:41:01 +1200

    Update TradieSignup.vue

--- front/src/views/TradieSignup.vue
+++ front/src/views/TradieSignup.vue
@@ -1,46 +1,44 @@
 <script setup>
 import InputText from "primevue/inputtext";
 import Checkbox from "primevue/checkbox";
+import Button_Main from "../components/Button.vue";
 </script>
 
 <template>
-  <section class="help heading-container">
-    <h1>How Find A Tradie Works</h1>
+  <!-- Heading Container -->
+  <section class="flex-col align-items-center">
+    <div class="flex-row">
+      <h1>How</h1>
+      <h1 class="orange-text">Find A Tradie</h1>
+      <h1>Works</h1>
+    </div>
     <p>Paste Katie's Component Here.....</p>
   </section>
+
+  <!-- Start Of Sign Up -->
   <section class="sign-up">
     <div class="sign-up-heading">
       <h1>Sign Up</h1>
     </div>
 
+    <!-- Image Drop -->
+
     <div class="image-drop grid-work">
-      <h1>Image Drop</h1>
-    </div>
-
-    <div class="input grid-work">
-      <inputText />
-    </div>
-
-    <div class="tradie-key-words">
-      <h1>kys</h1>
-    </div>
-  </section>
-
-  <div class="client-signup">
-    <div class="margin1 flex">
-      <div class="links-div">
-        <router-link to="/" class="spacing-large link">Go to Home</router-link>
+      <div class="flex-col align-items-center">
+        <h1>Image Drop 1</h1>
+        <Button_Main label="Upload Logo" />
+      </div>
+      <div class="flex-col align-items-center">
+        <h1>Image Drop 2</h1>
+        <Button_Main label="Upload Logo" />
       </div>
     </div>
 
-    <div class="flex">
-      <div class="flex-col">
-        <div class="heading-container">
-          <div class="spacing-large align-items-center">
-            <h1>Sign Up</h1>
-          </div>
-        </div>
+    <!-- Input Fields -->
 
+    <div class="input grid-work">
+      <div class="input-field-1">
+        <h1>Input Field 1</h1>
         <div class="text-box-container">
           <div class="spacing-standard">
             <p>First Name</p>
@@ -57,15 +55,6 @@ import Checkbox from "primevue/checkbox";
               v-model="Lname"
               class="input-class-box"
               placeholder="Last Name"
-            />
-          </div>
-
-          <div class="spacing-standard">
-            <p>Location</p>
-            <InputText
-              v-model="location"
-              class="input-class-box align-items-center"
-              placeholder="Location"
             />
           </div>
 
@@ -92,31 +81,71 @@ import Checkbox from "primevue/checkbox";
             <InputText class="input-class-box" placeholder="Confirm Password" />
           </div>
         </div>
+      </div>
+      <div class="input-field-2">
+        <h1>Input Field 2</h1>
 
-        <div class="checkbox-container">
-          <div class="spacing-standard flex-row">
-            <Checkbox class="" v-model="tos_concent" />
-            <p>Do You Consent To The Terms Of Service</p>
-          </div>
-        </div>
-
-        <div class="checkbox-container">
-          <div class="spacing-standard flex-row">
-            <Checkbox class="" v-model="email_concent" />
-            <p>Do You Concent To Recieving Emails From Find A Tradie</p>
-          </div>
-        </div>
-
-        <div class="checkbox-container">
+        <div class="text-box-container">
           <div class="spacing-standard">
-            <p>Insert Kelsie's Button Here</p>
+            <p>Business Name</p>
+            <InputText
+              v-model="business_name"
+              class="input-class-box"
+              placeholder="Business Name"
+            />
+          </div>
+
+          <div class="spacing-standard">
+            <p>Business Phone Number</p>
+            <InputText
+              v-model="business_phone_number"
+              class="input-class-box"
+              placeholder="Business Phone Number"
+            />
+          </div>
+
+          <div class="spacing-standard">
+            <p>Business Website</p>
+            <InputText
+              v-model="business_website"
+              class="input-class-box"
+              placeholder="Business Website"
+            />
+          </div>
+
+          <div class="spacing-standard">
+            <p>Write A Short Description Of Your Business</p>
+            <InputText
+              v-model="business_bio"
+              class="input-class-box"
+              placeholder="Max 1000 Words"
+            />
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <h1>CLIENT SIGN UP FOOTER AREA</h1>
+    <!-- Traide Key Words -->
+
+    <div class="tradie-key-words flex-row">
+      <div>
+        <p>Certification Number</p>
+        <InputText class="input-class-box" />
+      </div>
+      <div>
+        <p>Trade Title</p>
+        <InputText class="input-class-box" />
+      </div>
+      <div>
+        <p></p>
+        <Button_Main label="Add" class="disabled" />
+      </div>
+    </div>
+  </section>
+
+  <!-- Must Delete Below -->
+
+  <p>Idek why but this is showing up red</p>
 </template>
 
 <script>
@@ -132,10 +161,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.orange-text {
+  color: var(--color-brand);
+  margin: auto 0;
+}
 .grid-work {
   display: grid;
-  grid-template-columns: (4, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   gap: var(--spacing-standard);
+  border: 3px solid magenta;
+}
+.sign-up-heading {
+  display: flex;
+  justify-content: center;
 }
 
 p {
@@ -145,24 +183,11 @@ p {
 h1 {
   font-family: var(--font-main);
 }
-.heading-container {
-  max-width: 800px;
-  margin: 0 auto;
-}
+
 .text-box-container {
   max-width: 500px;
   width: 100%;
   margin: 0 auto;
-}
-.checkbox-container {
-  max-width: 500px;
-  margin: 0 auto;
-}
-// .align-items-center{
-//     align-items: center;
-// }
-.link {
-  color: black;
 }
 
 .input-class-box {
@@ -184,11 +209,6 @@ h1 {
   display: flex;
   flex-direction: column;
   flex: 1;
-}
-.flex {
-  display: flex;
-  flex: 1;
-  border: 3px solid cyan;
 }
 .spacing-small {
   padding-bottom: var(--spacing-small);
