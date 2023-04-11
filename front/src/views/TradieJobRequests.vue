fa24d4dc33b405222bccac3c18e0f4d3090b61d6
kelsiefromyoobee <kelsie.baigent@gmail.com>
2023-04-12 09:57:13 +1200

    added in progress

--- front/src/views/TradieJobRequests.vue
+++ front/src/views/TradieJobRequests.vue
@@ -2,12 +2,14 @@
     <TradieJobProcess/>
     <PendingRequests/>
     <QuoteSent/>
+    <InProgress/>
 </template>
 
 <script setup>
  import TradieJobProcess from '../components/TradieJobProcess.vue';
  import PendingRequests from '../components/PendingRequests.vue';
  import QuoteSent from '../components/QuoteSent.vue';
+ import InProgress from '../components/InProgress.vue';
 </script>
 
 <style scoped>
