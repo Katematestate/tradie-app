399c6c740bfb37ea946be23e0d951e33b75a545e
thewhitemj <113862023+thewhitemj@users.noreply.github.com>
2023-04-12 08:45:59 +1200

    Login Modal Changing

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,13 +2,17 @@
 import DynamicDialog from "primevue/dynamicdialog";
 import NavBar from "./components/NavBar.vue";
 import Footer from "./components/Footer.vue";
+import LoginModal from "./components/LoginModal.vue";
 </script>
 
 <template>
-  <NavBar />
-  <router-view></router-view>
-  <DynamicDialog />
-  <Footer />
+  <div>
+    <NavBar />
+    <LoginModal />
+    <router-view></router-view>
+    <DynamicDialog />
+    <Footer />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ front/src/components/LoginModal.vue
@@ -0,0 +1,36 @@
+<script setup>
+import InputText from "primevue/inputtext";
+import Button_Main from "./Button.vue";
+</script>
+
+<template>
+  <div class="modal-container">
+    <div>
+      <h1>Log In</h1>
+    </div>
+    <InputText v-model="email" class="input-class-box" placeholder="Email" />
+    <InputText
+      v-model="password"
+      type="password"
+      class="input-class-box"
+      placeholder="Password"
+    />
+    <Button_Main label />
+  </div>
+</template>
+
+<style>
+.modal-container {
+  border: black solid 3px;
+}
+.text-box-container {
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  gap: var(--spacing-standard);
+
+  @media (max-width: 425px) {
+    padding: 0 var(--spacing-standard);
+  }
+}
+</style>
