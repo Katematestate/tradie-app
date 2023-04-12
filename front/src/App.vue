15e14ce15c09a87c268c48708b28d75daab38324
Katematestate <80445606+Katematestate@users.noreply.github.com>
2023-04-12 22:15:34 +1200

    show login modal on auth failure

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,25 +3,26 @@ import DynamicDialog from "primevue/dynamicdialog";
 import ConfirmPopup from "primevue/confirmpopup";
 import NavBar from "./components/NavBar.vue";
 import Footer from "./components/Footer.vue";
-import LoginModal from "./components/LoginModal.vue";
 </script>
 
 <template>
-  <div>
-    <LoginModal @toggle_modal="toggleModal" v-if="showModal" />
-    <NavBar @toggle_modal="toggleModal" />
-    <router-view></router-view>
-    <Footer />
-    <DynamicDialog />
-    <ConfirmPopup />
-  </div>
+  <!-- <LoginModal @toggle_modal="toggleModal" v-if="showModal" /> -->
+  <NavBar @toggle_modal="toggleModal" />
+  <router-view></router-view>
+  <Footer />
+  <DynamicDialog />
+  <ConfirmPopup />
 </template>
 
 <script>
+import { useDialog } from "primevue/usedialog";
+import LoginModal from "./components/LoginModal.vue";
+
 export default {
   data() {
     return {
       showModal: false,
+      dialog: useDialog(),
     };
   },
   methods: {
@@ -32,7 +33,8 @@ export default {
   },
   created() {
     window.addEventListener("show-login-modal", () => {
-      this.showModal = true;
+      // this.showModal = true;
+      this.dialog.open(LoginModal, { props: { modal: true, header: "login" } });
     });
   },
 };
