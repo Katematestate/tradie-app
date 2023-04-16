0abb1f31ade4f2ce358aae6c3c7a3beb6bb894a3
Merlin Stemmer <stemmermerlin@gmail.com>
2023-04-16 17:00:10 +1200

    Update listJobsByUserOrBusiness.js
    fix job filtering on client hub/tradie request pages

--- back/controllers/job/listJobsByUserOrBusiness.js
+++ back/controllers/job/listJobsByUserOrBusiness.js
@@ -1,23 +1,23 @@
 const Job = require("../../models/job");
 
 const listJobsByUserOrBusiness = async (req, res) => {
-    try {
-        const { userId, businessId } = req.params;
-        const query = {};
+  try {
+    const { userId, businessId } = req.query;
+    const query = {};
 
-        if (userId) {
-            query.user = userId;
-        }
-        if (businessId) {
-            query.business = businessId;
-        }
-
-        const jobs = await Job.find(query);
-        res.json(jobs)
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Error getting jobs" });
+    if (userId) {
+      query.user = userId;
     }
-}
+    if (businessId) {
+      query.business = businessId;
+    }
+
+    const jobs = await Job.find(query);
+    res.json(jobs);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error getting jobs" });
+  }
+};
 
 module.exports = listJobsByUserOrBusiness;
